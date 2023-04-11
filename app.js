a1723be31f1e73a8e974c860a5ea40cab5c0afe4
Aziz Bakirov <azizbakirov9@gmail.com>
2023-04-12 03:46:44 +0500

    new design

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const theme = document.querySelector(".toogle");
 const icon = document.querySelector(".fa-moon");
 const toggle = document.querySelector('.toogle')
+const body = document.querySelector('body')
 
 theme.addEventListener("click", () => {
   icon.classList.toggle("fa-moon");
   icon.classList.toggle("fa-sun");
   if(icon.classList.contains("fa-sun")){
-    toggle.style.backgroundColor = "#CFCFCF";
-    icon.style.color = "#CFCFCF";
-    icon.style.backgroundColor = "#101010";
+    toggle.style.backgroundColor = "#101010";
+    icon.style.color = "black";
+    icon.style.backgroundColor = "#cfcfcf";
+    body.style.backgroundColor = "#cfcfcf";
+    body.style.color = "#101010";
   }else{
+    body.style.color = "#cfcfcf";
+    body.style.backgroundColor = "#101010";
     toggle.style.backgroundColor = "#292929";
   }
 });
