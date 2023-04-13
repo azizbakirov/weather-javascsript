b46146df4e256ea9b72f7e746347bdd14fee45b8
Aziz Bakirov <azizbakirov9@gmail.com>
2023-04-13 23:41:05 +0500

    css succes

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
 const theme = document.querySelector(".toogle");
-const icon = document.querySelector(".fa-moon");
-const toggle = document.querySelector('.toogle')
-const body = document.querySelector('body')
+const icon = document.querySelector(".fa-sun");
+const toggle = document.querySelector(".toogle");
+const body = document.querySelector("body");
+const footer = document.querySelector("footer");
+const todayLight = document.querySelector(".today");
+const items = document.querySelectorAll(".items, .region");
+const mapTemp = document.querySelector(".map_temp");
+const weatherIcon = document.querySelectorAll(".img_icons");
+const inp = document.querySelector('input')
+const socialIcon = document.querySelectorAll(".social_nav");
+
+console.log(socialIcon);
+
 
 theme.addEventListener("click", () => {
   icon.classList.toggle("fa-moon");
   icon.classList.toggle("fa-sun");
-  if(icon.classList.contains("fa-sun")){
-    toggle.style.backgroundColor = "#101010";
-    icon.style.color = "black";
-    icon.style.backgroundColor = "#cfcfcf";
-    body.style.backgroundColor = "#cfcfcf";
-    body.style.color = "#101010";
-  }else{
-    body.style.color = "#cfcfcf";
-    body.style.backgroundColor = "#101010";
-    toggle.style.backgroundColor = "#292929";
-  }
+  body.classList.toggle("active");
+  themes();
 });
+
+function themes() {
+  if (icon.classList.contains("fa-moon")) {
+    items.forEach((item) => {
+      item.style.backgroundColor = "#637e94bb";
+    });
+    socialIcon.forEach(item =>{
+      item.style.backgroundColor = "#637e94bb";
+    })  
+    inp.style.backgroundColor = "#637e94bb";
+    inp.style.color = "black";
+    toggle.style.backgroundColor = "#637e94bb";
+    mapTemp.style.backgroundColor = "#637e94bb";
+    todayLight.style.backgroundColor = "#637e94bb";
+    footer.style.backgroundColor = "#637e94bb";
+    footer.style.color = "black";
+    console.log("light");
+  } else {
+    items.forEach((item) => {
+      item.style.backgroundColor = "#292929";
+    });
+    socialIcon.forEach((item) => {
+      item.style.backgroundColor = "#292929";
+    });  
+    inp.style.backgroundColor = "#292929";
+    inp.style.color = "white";
+    toggle.style.backgroundColor = "#292929";
+    mapTemp.style.backgroundColor = "#292929";
+    todayLight.style.backgroundColor = "#bcd8edbb";
+    footer.style.backgroundColor = "#292929";
+    footer.style.color = "white";
+    console.log("dark");
+  }
+}
